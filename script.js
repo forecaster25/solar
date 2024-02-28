fef458ef2dfcd5ea55e503fdaddd5e38606ce0d8
forecaster25 <93544566+forecaster25@users.noreply.github.com>
2024-02-28 08:30:19 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,185 +1,314 @@
-    // Fetch data from the API
-    fetch('https://services.swpc.noaa.gov/json/goes/primary/xrays-6-hour.json')
-    .then(response => response.json())
-    .then(data => {
-      const filteredData = data.filter(entry => entry.energy === '0.1-0.8nm');
 
-      const timestamps = filteredData.map(entry => new Date(entry.time_tag).getTime());
-      const fluxValues = filteredData.map(entry => entry.flux);
 
-      const minFlux = Math.min(...fluxValues);
-      const maxFlux = Math.max(...fluxValues);
-      const updateTime = new Date(Math.max(...timestamps));
+/**
+ * In order to synchronize tooltips and crosshairs, override the
+ * built-in events with handlers defined on the parent element.
+ */
+['mousemove', 'touchmove', 'touchstart'].forEach(function (eventType) {
+  document.getElementById('chart1').addEventListener(
+      eventType,
+      function (e) {
+          let chart,
+              point,
+              i,
+              event;
 
-      console.log('Min Flux',minFlux)
-      console.log('Most Recent Updated Time:', updateTime)
+          for (i = 0; i < Highcharts.charts.length; i = i + 1) {
+              chart = Highcharts.charts[i];
+              // Find coordinates within the chart
+              event = chart.pointer.normalize(e);
+              // Get the hovered point
+              point = chart.series[0].searchPoint(event, true);
 
-      Highcharts.chart('xrayChart', {
-        chart: {
-          type: 'spline',
-          backgroundColor: '#212121',
-          borderRadius: 8,
-          plotBorderWidth: 0
-        },
-        height: 1200,
-        rangeSelector: {
-          enabled: false,
-          selected: 1
-        },
-        title: {
-          text: 'Flux Over Last 6 Hours - Last Updated Time: ' + updateTime,
-          style: {
-            color: '#FFFFFF',
-            fontSize: '10px',
-            //fontWeight: 'bold'
-          }
-        },
-        xAxis: {
-          type: 'datetime',
-          labels: {
-            format: '{value:%H:%M}',
-            style: {
-              color: '#FFFFFF' // Make x-axis labels white
-            },
-          },
-          style: {
-            color: '#FFFFFF' // Make x-axis labels white
-          },
-          lineColor: '#FFFFFF'
-        },
-        yAxis: {
-          type: 'logarithmic',
-          //minorTickInterval: 0.1,
-          //tickColor: 'red',
-          offset: 0,
-          title: {
-            text: 'Flux',
-            style: {
-              color: '#FFFFFF' // Make y-axis title white
-            }
-          },
-          tickColor: 'white',
-          tickPositions: [-8, -7, -6, -5, -4, -3],
-          labels: {
-            style: {
-                color: '#FFFFFF' // Make y-axis labels white
-              },
-            formatter: function() {
-              const value = Math.pow(10, this.value);
-              if (this.pos === -8) {
-                return 'A';
-              } else if (this.pos === -7) {
-                return 'B';
-              } else if (this.pos === -6) {
-                return 'C';
-              } else if (this.pos === -5) {
-                return 'M';
-              } else if (this.pos === -4) {
-                return 'X';
-              } else if (this.pos > -4) {
-                return '';
-              } else {
-                return value.toExponential();
+              if (point) {
+                  point.highlight(e);
               }
-            }
+          }
+      }
+  );
+});
+['mousemove', 'touchmove', 'touchstart'].forEach(function (eventType) {
+  document.getElementById('chart2').addEventListener(
+      eventType,
+      function (e) {
+          let chart,
+              point,
+              i,
+              event;
+
+          for (i = 0; i < Highcharts.charts.length; i = i + 1) {
+              chart = Highcharts.charts[i];
+              // Find coordinates within the chart
+              event = chart.pointer.normalize(e);
+              // Get the hovered point
+              point = chart.series[0].searchPoint(event, true);
+
+              if (point) {
+                  point.highlight(e);
+              }
+          }
+      }
+  );
+});
+['mousemove', 'touchmove', 'touchstart'].forEach(function (eventType) {
+  document.getElementById('chart3').addEventListener(
+      eventType,
+      function (e) {
+          let chart,
+              point,
+              i,
+              event;
+
+          for (i = 0; i < Highcharts.charts.length; i = i + 1) {
+              chart = Highcharts.charts[i];
+              // Find coordinates within the chart
+              event = chart.pointer.normalize(e);
+              // Get the hovered point
+              point = chart.series[0].searchPoint(event, true);
+
+              if (point) {
+                  point.highlight(e);
+              }
+          }
+      }
+  );
+});
+/**
+* Override the reset function, we don't need to hide the tooltips and
+* crosshairs.
+*/
+Highcharts.Pointer.prototype.reset = function () {
+  return undefined;
+};
+
+/**
+* Highlight a point by showing tooltip, setting hover state and draw crosshair
+*/
+Highcharts.Point.prototype.highlight = function (event) {
+  event = this.series.chart.pointer.normalize(event);
+  this.onMouseOver(); // Show the hover marker
+  this.series.chart.tooltip.refresh(this); // Show the tooltip
+  this.series.chart.xAxis[0].drawCrosshair(event, this); // Show the crosshair
+};
+
+//Main body to collect and plot data
+document.addEventListener('DOMContentLoaded', function () {
+  // Fetch data from the JSON source
+  fetch('https://services.swpc.noaa.gov/json/rtsw/rtsw_mag_1m.json')
+  .then(response => response.json())
+  .then(data => {
+      // Filter data by 'active': true
+      const activeData = data.filter(entry => entry.active === true);
+
+      // Process data for the first chart
+      const timeSeries1 = activeData.map(entry => [
+          new Date(entry.time_tag).getTime(),
+          entry.bt
+      ]);
+
+      // Process data for the second chart
+      const timeSeries2 = activeData.map(entry => [
+          new Date(entry.time_tag).getTime(),
+          entry.bz_gse
+      ]);
+
+      // Process data for the third chart
+      const timeSeries3 = activeData.map(entry => [
+          new Date(entry.time_tag).getTime(),
+          entry.phi_gse
+      ]);       
+
+      // Create first chart with formatted time labels
+      Highcharts.chart('chart1', {
+          chart: {
+            type: 'spline',
+            backgroundColor: '#212121',
+            borderRadius: 8,
+            plotBorderWidth: 0,
+            syncId: 'syncGroup'
           },
-          gridLineColor: 'white',
-        },
-        tooltip: {
-          formatter: function () {
-            const flux = this.y;
-            let unit;
-            if (flux >= 1e-8 && flux < 1e-7) {
-              unit = 'A';
-            } else if (flux >= 1e-7 && flux < 1e-6) {
-              unit = 'B';
-            } else if (flux >= 1e-6 && flux < 1e-5) {
-              unit = 'C';
-            } else if (flux >= 1e-5 && flux < 1e-4) {
-              unit = 'M';
-            } else if (flux >= 1e-4) {
-              unit = 'X';
-            }
-            return unit + formatExponential(flux, 2);
-          }
-        },
-        plotOptions: {
-          area:{
-            fillColor: {
-              linearGradient:{
-                x1: 0,
-                y1: 0,
-                x2: 0,
-                y2: 1,
-              },
-              stops: [
-                        [0, '#590203'], //Highcharts.getOptions().colors[4]
-                        [1, Highcharts.color(Highcharts.getOptions().colors[7]).setOpacity(0).get('rgba')], 
-                    ]
-            }
+          title: {
+              text: 'bt',
+              align: 'left'
           },
-          threshold: null
-          //spline: {
-          //  lineWidth: 5 // Make the line wider
-          //}
-        },
-        legend: {
-          enabled: false,
-          itemStyle: {
-            color: '#FFFFFF' // Make legend text white
-          }
-        },
-        series: [{
-          type: 'area',
-          name: 'Flux',
-          data: fluxValues.map((value, index) => [timestamps[index], value]),
-          color: '#a31702'//{
-          //linearGradient: { x1: 0, x2: 0, y1: 0, y2: 1 },
-          //stops: [
-          //    [0, '#f52505'], // Start color (red)
-          //    [0.5, '#521910'], // Middle color (yellow)
-          //    [1, '#000000'] // End color 
-          //  ]
-          }
-        ],
-        exporting: {
-            enabled: false,
-            buttons: {
-                contextButton: {
-                  symbolFill: '#121212',
-                  symbolStroke: 'white',
-                  theme: {
-                      fill: '#121212',
-                      stroke: 'white',
-                      states: {
-                          hover: {
-                              fill: '#474747',
-                              stroke: 'white'
-                          },
-                          select: {
-                              fill: '#474747',
-                              stroke: 'white'
-                          }
-                      }
+          legend: {
+              enabled: false
+          },
+          xAxis: {
+              type: 'datetime',
+              crosshair: true,
+              color: 'black',
+              gridLineWidth: 0,
+              labels: {
+                  formatter: function () {
+                      return Highcharts.dateFormat('%H:%M:%S', this.value);
                   }
-                }
-            }
-        }
+              }
+          },
+          yAxis:{
+              gridLineWidth: 0.1
+          },
+          plotOptions: {
+              area: {
+                  fillColor: {
+                      linearGradient: {
+                      x1: 0,
+                      y1: 0,
+                      x2: 0,
+                      y2: 1
+                      },
+                      stops: [
+                      [0, Highcharts.getOptions().colors[0]],
+                      [1, Highcharts.color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                      ],
+                      'min': -20,
+                      'max': 20
+
+                  }
+              }
+          },
+          series: [{
+              type: 'area',
+              name: 'bt',
+              data: timeSeries1
+          }],
+          exporting: {
+              enabled: false
+          }
       });
-    });
 
-function formatExponential(number, decimalPlaces) {
-    // Convert the number to a string
-    let numString = number.toExponential().toString();
+      // Create second chart with formatted time labels
+      Highcharts.chart('chart2', {
+          chart: {
+            type: 'spline',
+            backgroundColor: '#212121',
+            borderRadius: 8,
+            plotBorderWidth: 0,
+            syncId: 'syncGroup'
+          },
+          title: {
+              text: 'bz',
+              align: 'left'
+          },
+          legend: {
+              enabled: false
+          },
+          xAxis: {
+              type: 'datetime',
+              crosshair: true,
+              labels: {
+                  formatter: function () {
+                      return Highcharts.dateFormat('%H:%M:%S', this.value);
+                  }
+              }
+          },
+          yAxis:{
+              gridLineWidth: 0.1
+          },
+          plotOptions: {
+              area: {
+                  fillColor: {
+                      linearGradient: {
+                      x1: 0,
+                      y1: 0,
+                      x2: 0,
+                      y2: 1
+                      },
+                      stops: [
+                      [0, '#006e1d'],
+                      [.5, '#212121FF'],
+                      [1, '#9c0202']
+                      ],
+                      'min': -20,
+                      'max': 20
 
-    // Split the string into coefficient and exponent parts
-    let parts = numString.split('e');
-    let coefficient = parseFloat(parts[0]);
-    let exponent = parseInt(parts[1]);
+                  }
+              }
+          },
+          series: [{
+              type: 'area',
+              name: 'bz',
+              color: {
+                  linearGradient: { x1: 0, x2: 0, y1: 0, y2: 1 },
+                  stops: [
+                      [0, 'green'],
+                      [0.5, '#212121CC'],
+                      [1, 'red']
+                  ],
+              },
+              data: timeSeries2,
+              marker: {
+                  radius: 5
+              }
+          }],
+          exporting: {
+              enabled: false
+          }
+      });
 
-    // Format the coefficient with the specified decimal places
-    let formattedCoefficient = coefficient.toFixed(decimalPlaces);
+      // Create third chart with formatted time labels
+      Highcharts.chart('chart3', {
+          chart: {
+            type: 'scatter',
+            backgroundColor: '#212121',
+            borderRadius: 8,
+            plotBorderWidth: 0,
+            syncId: 'syncGroup'
+          },
+          title: {
+              text: 'phi',
+              align: 'left'
+          },
+          legend: {
+              enabled: false
+          },
+          xAxis: {
+              type: 'datetime',
+              crosshair: true,
+              color: 'black',
+              gridLineWidth: 0,
+              labels: {
+                  formatter: function () {
+                      return Highcharts.dateFormat('%H:%M:%S', this.value);
+                  }
+              }
+          },
+          yAxis:{
+              gridLineWidth: 0.1
+          },
+          plotOptions: {
+              area: {
+                  fillColor: {
+                      linearGradient: {
+                      x1: 0,
+                      y1: 0,
+                      x2: 0,
+                      y2: 1
+                      },
+                      stops: [
+                      [0, Highcharts.getOptions().colors[4]],
+                      [1, Highcharts.color(Highcharts.getOptions().colors[4]).setOpacity(0).get('rgba')]
+                      ],
+                      'min': -20,
+                      'max': 20
 
-    // Combine the formatted coefficient and exponent with 'e'
-    return formattedCoefficient
-}
+                  }
+              }
+          },
+          series: [{
+              //type: 'area',
+              name: 'bt',
+              data: timeSeries3,
+              marker: {
+                  radius: 1
+              }
+          }],
+          exporting: {
+              enabled: false
+          }
+      });
+  })
+  .catch(error => {
+      console.error('Error fetching data:', error);
+  });
+});
